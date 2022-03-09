3fb049ab9615b9a0a530bfa84c43c76759ab6472
bf319 <bogdan@DESKTOP-I2ANCVS.localdomain>
2022-03-09 22:54:33 +0000

    Copy code from models.py

--- models.py
+++ models.py
@@ -1,9 +1,26 @@
+# Copyright 2021 DeepMind Technologies Limited.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""MAG240M-LSC models."""
+
 from typing import Callable, NamedTuple, Sequence
 
 import haiku as hk
 import jax
 import jax.numpy as jnp
 import jraph
+
 
 _REDUCER_NAMES = {
     'sum':
@@ -14,7 +31,14 @@ _REDUCER_NAMES = {
         jraph.segment_softmax,
 }
 
-# From https://github.com/deepmind/deepmind-research/blob/master/ogb_lsc/mag/models.py#L42
+
+class ModelOutput(NamedTuple):
+  node_embeddings: jnp.ndarray
+  node_embedding_projections: jnp.ndarray
+  node_projection_predictions: jnp.ndarray
+  node_logits: jnp.ndarray
+
+
 def build_update_fn(
     name: str,
     output_sizes: Sequence[int],
@@ -54,7 +78,7 @@ def build_update_fn(
 
   return single_mlp(f'{name}_homogeneous')
 
-# From https://github.com/deepmind/deepmind-research/blob/master/ogb_lsc/mag/models.py#L82
+
 def build_gn(
     output_sizes: Sequence[int],
     activation: Callable[[jnp.ndarray], jnp.ndarray],
@@ -99,6 +123,7 @@ def build_gn(
       include_sent_messages_in_node_update=use_sent_edges,
   )
 
+
 def _get_activation_fn(name: str) -> Callable[[jnp.ndarray], jnp.ndarray]:
   if name == 'identity':
     return lambda x: x
@@ -108,11 +133,6 @@ def _get_activation_fn(name: str) -> Callable[[jnp.ndarray], jnp.ndarray]:
                    'See https://jax.readthedocs.io/en/latest/jax.nn.html'
                    'for the list of supported function names.')
 
-class ModelOutput(NamedTuple):
-  node_embeddings: jnp.ndarray
-  node_embedding_projections: jnp.ndarray
-  node_projection_predictions: jnp.ndarray
-  node_logits: jnp.ndarray
 
 class NodePropertyEncodeProcessDecode(hk.Module):
   """Node Property Prediction Encode Process Decode Model."""
